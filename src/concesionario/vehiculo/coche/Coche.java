47057b43e46941274f2fcd299a652dfc0e8cde50
Raquelfefer <raquelfefer@gmail.com>
2025-03-07 12:50:26 +0100

    cambio

--- src/concesionario/vehiculo/coche/Coche.java
+++ src/concesionario/vehiculo/coche/Coche.java
@@ -1,5 +1,25 @@
 package concesionario.vehiculo.coche;
 
-public class Coche {
+import concesionario.vehiculo.Vehiculo;
 
+public abstract class Coche extends Vehiculo{
+
+	private TipoCombustible combustible;
+
+	public Coche(String marca, String modelo, String matricula, String combustible) {
+		super(marca, modelo, matricula);
+		setCombustible(combustible);
+	}
+
+	public String getCombustible() {
+		return combustible.toString();
+	}
+
+	public void setCombustible(String combustible) {
+		this.combustible = TipoCombustible.valueOf(combustible);
+	}
+	
+	
+	
+	
 }
